b47cad8c2fdf0b9ba5432ac6ac6c888116fbaaab
Lynxie <AinonLynx@users.noreply.github.com>
2016-12-29 13:36:24 +0200

    Update jokes.py
    Increase joke interval

--- handlers/jokes.py
+++ handlers/jokes.py
@@ -5,6 +5,6 @@ lastTime = 0
 def stilli(bot, update):
     global lastTime
     current_time = time.time()
-    if (current_time - lastTime) > 300: #не шутить слишком часто
+    if (current_time - lastTime) > 3000: #не шутить слишком часто
         lastTime = current_time
         update.message.reply_text('Стилли аццтой!(c) :)')
