ad5cca7bad148e0dffc248fce21e649866b73f77
Prof Genius <prohfesor@gmail.com>
2018-03-16 15:22:17 +0200

    ukrainian Stilli (#8)
    * ukrainian Stilli
    
    * add non-word character match

--- fleaBot.py
+++ fleaBot.py
@@ -76,7 +76,7 @@ def main():
     dp.add_handler(RegexHandler(u'^\/delete(\d+).*', delete.delete_item, pass_groups=True))
 
     # Другое
-    dp.add_handler(RegexHandler(u'.*(\s|^)(С|с)тил{1,2}и(\s|$).*', jokes.stilli))
+    dp.add_handler(RegexHandler(u'.*(\s|\W|^)(С|с)т(и|і)л{1,2}(и|і)(\s|\W|$).*', jokes.stilli))
     dp.add_handler(CommandHandler("support", support.support))
 
     # log all errors
